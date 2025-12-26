a8d8dfd444b1389a41f30989c440663d1ff847d9
Forte-NaoP <Forte-NaoP@users.noreply.github.com>
2025-12-26 12:37:44 +0000

    Deploying to gh-pages from @ Forte-NaoP/forte-naop.github.io@5ca3bb41a48137ae06037ed0a6e8f6e8afdd1a57 🚀

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1766730560',resources: [
+    cacheName: 'chirpy-1766752663',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
